08a6d23bc6123f765e73156022a881c42173d299
Pa-Larson <52116121+Pa-Larson@users.noreply.github.com>
2020-04-20 20:15:48 -0500

    Formatting
    - Text fields now display properly (Create notes and Main Menu)
    - Window titles now represent their repsective title
    - Selecting a item in the main menu will select it's Note ID.

--- SE341/UserInterface.py
+++ SE341/UserInterface.py
@@ -1,12 +1,12 @@
 from tkinter import *
+
 import pymysql.cursors
 import Note
 import NoteList
 from functools import partial
 ## login screen
-#global username
-#username = StringVar()
-
+# global username
+# username = StringVar()
 
 
 def loginScreen():
@@ -15,7 +15,8 @@ def loginScreen():
 
     #global screen  # Makes a screen
     screen = Tk()  # Declares it as a Tk GUI
-    screen.geometry("300x300")  # The dimensions
+    screen.geometry("250x225")  # The dimensions
+    screen.title("Login")
 
     # Variable for the inputed Username
     global username  # Stores the user's name input
@@ -62,32 +63,27 @@ def loginButtonClicked(PwEntryObject, userPW, screen,usernameEntryObject):
 def mainMenu(root):
     root.destroy()
 
-
     #notelist = NoteList.NoteList()
     notes = notelist.getAllNotes()
 
-
-
     screen = Tk()  # Declares it as a Tk GUI
     screen.geometry("600x500")  # The dimensions
+    screen.title("Main Menu")
 
     ##Create listbox add all note entries to it
     global listbox
-    listbox = Listbox(screen, borderwidth=5,font = 'arial', relief = 'groove', selectbackground = 'lightgrey', height = 12  )
-
+    listbox = Listbox(screen, borderwidth=5, font='Courier', relief='groove', selectbackground='lightgrey', height=12)
     firstline = formatText("STUDENT NAME", "STUDENT ID", "NOTE ID")
-    #listbox.insert(END, firstline)
     for n in notes:
         theStudent = notelist.getStudentInfo(n.sID)
         studentName = theStudent.firstname
-        textline = 'Student name: ' + str(studentName) +    '        Student ID: ' +   str(n.sID) + '              Note ID: ' + str(n.noteID)
-        #textline = formatText(studentName, str(n.sID), str(n.noteID))
+        textline = formatText(studentName, str(n.sID), str(n.noteID))
         listbox.insert(END, textline)
 
     Label(screen, text="Select the note you would like to view", fg="black", font=("calibri", 14)).pack(anchor='n')
     Label(screen, text="").pack(anchor='n')
     Label(screen, text="").pack(anchor='n')
-    Label(screen, text="                 " + firstline, fg="black", font=("calibri", 12)).pack(anchor='n')
+    Label(screen, text="\t\t\t\t\t\t\t\t\t\t" + firstline, fg="black", font=("Courier", 12)).pack(anchor='n')
     listbox.pack(anchor='n')
     listbox.config(width=60)
     Label(screen, text=" ").pack(anchor='n')
@@ -98,9 +94,6 @@ def mainMenu(root):
     Label(screen, text=" ").pack(anchor='n')
     Button(screen, text="Create new note", width=20, height=1, pady=5,padx = 2, command=partial(createNote,screen)).pack(anchor='n')
 
-
-
-
     line = listbox.get(ANCHOR)
     print("The line contains: ",line)
     screen.update_idletasks()
@@ -108,31 +101,16 @@ def mainMenu(root):
     screen.mainloop()
 
 def formatText(StudentName, sID, nID):
-    s = "                                                                                                                   "
-    s = s[:20] + StudentName + s[20:]
-    s = s[:40] + sID + s[40:]
-    s = s[:70] + nID + s[70:]
-    print(s);
-
-    name1 = "Bob Stein"
-    name2 = "Noneya Business"
-    date1 = "11/22/3333"
-    date2 = "2/1/4444"
-    comp1 = "Yes"
-    comp2 = "No"
+    s = "                                                                                                              "
+    s = s[:2] + StudentName + s[2:]
+    s = s[:20] + sID + s[20:]
+    s = s[:35] + nID + s[35:]
     return(s)
-    format(name1, date1, comp1)
-    format(name2, date2, comp2)
 
 def getAnchor(root):
-
     line = listbox.get(ANCHOR)
     root.destroy()
-    keyword = 'Note ID: '
-    noteID = line.partition(keyword)[2]
-    noteID = int(noteID)
-
-
+    noteID = int(line[35:])
     viewNote(noteID)
 
 def createNote(root):
@@ -158,8 +136,6 @@ def createNote(root):
     b2.pack(side=RIGHT, padx=5, pady=5)
     b1 = Button(screen, text='   Go Back   ', command = partial(mainMenu, screen)).pack(side=RIGHT, padx=5, pady=5)
 
-
-
     screen.mainloop()
 
 # Retrieve the values contained in Tkinter entries and attempt to add them to DB
@@ -181,6 +157,7 @@ def viewNote(noteID):
     #root.destroy()
     screen = Tk()  # Declares it as a Tk GUI
     screen.geometry("500x500")  # The dimensions
+    screen.title("View Note")
 
     theNote = notelist.getNote(noteID)
     print("The note is: ", theNote.sID, " ", theNote.reason)
@@ -188,21 +165,21 @@ def viewNote(noteID):
 
     Label(screen, font=('arial',14,'bold'), pady=30, text="Student note details").pack(anchor='n')
 
-    listbox = Listbox(screen, borderwidth=5,fg = 'black', font='arial', relief='groove', selectbackground='lightgrey', height=12,width=45)
-    listbox.insert(END, "Student first name: " + theStudent.firstname)
-    listbox.insert(END, "Student last: " + theStudent.lastname)
-    listbox.insert(END, "Student ID: " + str(theStudent.sID))
-    listbox.insert(END, "Student phone: " + str(theStudent.phonenumber))
-    listbox.insert(END, "Student address: " + theStudent.address)
-    listbox.insert(END, "Note ID: " + theNote.noteID)
-    listbox.insert(END, "Reason for note: " + theNote.reason)
-    listbox.insert(END, "Additional notes: " + theNote.note)
-    listbox.insert(END, "Note is for: " + theNote.forWho)
-    listbox.insert(END, "Note made by: " )
-    listbox.insert(END, "Employee ID: " + str(theNote.eID))
-    listbox.insert(END, "Date created: ")
-    listbox.insert(END, "Date completed: ")
-    listbox.insert(END, "Status: ")
+    listbox = Listbox(screen, borderwidth=5,fg = 'black', font='Courier', relief='groove', selectbackground='lightgrey', height=12,width=45)
+    listbox.insert(END, "Student first name:   " + theStudent.firstname)
+    listbox.insert(END, "Student last:         " + theStudent.lastname)
+    listbox.insert(END, "Student ID:           " + str(theStudent.sID))
+    listbox.insert(END, "Student phone:        " + str(theStudent.phonenumber))
+    listbox.insert(END, "Student address:      " + theStudent.address)
+    listbox.insert(END, "Note ID:              " + theNote.noteID)
+    listbox.insert(END, "Reason for note:      " + theNote.reason)
+    listbox.insert(END, "Additional notes:     " + theNote.note)
+    listbox.insert(END, "Note is for:          " + theNote.forWho)
+    listbox.insert(END, "Note made by:         " )
+    listbox.insert(END, "Employee ID:          " + str(theNote.eID))
+    listbox.insert(END, "Date created:         ")
+    listbox.insert(END, "Date completed:       ")
+    listbox.insert(END, "Status:               ")
 
     listbox.pack(anchor='n')
 
@@ -224,10 +201,5 @@ def createGlobals():
     global notelist
     notelist = NoteList.NoteList()
 
-
 createGlobals()
 loginScreen()
-
-
-
-
